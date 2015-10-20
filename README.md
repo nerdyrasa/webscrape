94f1aef6ae7d2efbef751e48be32959ce09198d3
Roz Isaacson <rozisaacson@users.noreply.github.com>
2015-10-20 07:30:37 -0500

    Add functionality to extract individual player stats

--- README.md
+++ README.md
@@ -10,6 +10,10 @@ I used Python 2.7.10 with BeautifulSoup4.
 
 Run python webscrape.py.
 
+## Known Issues
+
+The program choked on the team San Jos&eacute State, therefore I used a small subset of teams that did not include San Jos&eacute State to test my program. I'll need to take a look at that.
+
 ## Attribution ##
 
 Writing Idiomatic Python Video One [youtube video](https://www.youtube.com/watch?v=g0gNWGg2JxM)
--- webscrape.py
+++ webscrape.py
@@ -10,12 +10,27 @@ def urls_for_teams(url):
     page = urllib2.urlopen(url).read()
     soup = BeautifulSoup(page, 'html.parser')
     url_list = soup.find(class_='select-box').find_all('option')
-    return [(element.text, element['value']) for element in url_list][2:]
+    return [(element.text, element['value']) for element in url_list][2:5]
+
+
+def player_list_from_url_map(team_urls):
+    """Return a list of lists of each of the players statistics"""
+    player_list = []
+    for (team_name, url) in team_urls:
+
+        team_page = urllib2.urlopen(url).read()
+
+        for row in BeautifulSoup(team_page, 'html.parser').find_all('tr')[2:]:
+            current_row = row.find_all('td')
+            player_list.append([e.text for e in current_row])
+
+    return player_list
 
 
 def main():
     """Main entry point for the script"""
     team_urls = urls_for_teams(_URL)
+    player_list = player_list_from_url_map(team_urls)
 
 
 if __name__ == '__main__':
