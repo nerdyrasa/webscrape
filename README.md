5d6d6de3d9da8bd45b6bf0d653547243b2ae3b9b
Roz Isaacson <rozisaacson@users.noreply.github.com>
2015-12-21 18:01:36 -0600

    Handle non-ascii url; update code from Python 2.7 to Python 3.4

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ This is a simple web scraping program using BeautifulSoup4.
 
 ## Requirements
 
-I used Python 2.7.10 with BeautifulSoup4.
+Python 3.4.2
+BeautifulSoup4 4.4.1
 
 ## Usage
 
@@ -12,7 +13,17 @@ Run python webscrape.py.
 
 ## Known Issues
 
-The program choked on the team San Jos&eacute; State, therefore I used a small subset of teams that did not include San Jos&eacute; State to test my program. I'll need to take a look at that.
+Initially, the program generated the following error for the team San Jos&eacute; State:
+
+UnicodeEncodeError: 'ascii' codec can't encode character u'\xe9' in position 49: ordinal not in range(128)
+
+This was a great opportunity for me to learn more about Unicode:
+http://www.joelonsoftware.com/articles/Unicode.html
+
+Although the code has been updated to handle the non-ascii url, the csv file doesn't seem to be handling it properly.
+I still need to handle the write to the csv file. Also, there is an extra unnecessary line between player stats.
+This probably has something to do with the fact that my current dev env is Windows 10.
+
 
 ## Attribution ##
 
--- webscrape.py
+++ webscrape.py
@@ -1,25 +1,45 @@
 import csv
 import sys
+import urllib.request, urllib.parse
+
 from bs4 import BeautifulSoup
-import urllib2
+
 
 _URL = 'http://espn.go.com/college-football/team/roster/_/id/2132/cincinnati-bearcats'
-
+_HDR = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+        'Accept-Encoding': 'none',
+        'Accept-Language': 'en-US,en;q=0.8',
+        'Connection': 'keep-alive'}
 
 def urls_for_teams(url):
+
+    #create a request object
+    req = urllib.request.Request(url, headers=_HDR)
+
+
     """Return a list of tuples containing team name and its associated url"""
-    page = urllib2.urlopen(url).read()
+    page = urllib.request.urlopen(req).read()
     soup = BeautifulSoup(page, 'html.parser')
     url_list = soup.find(class_='select-box').find_all('option')
-    return [(element.text, element['value']) for element in url_list][2:5]
+    return [(element.text, element['value']) for element in url_list][2:]
 
 
 def player_list_from_url_map(team_urls):
     """Return a list of lists of each of the players statistics"""
-    player_list = []
-    for (team_name, url) in team_urls:
 
-        team_page = urllib2.urlopen(url).read()
+    player_list = []
+    for (team_name, url) in team_urls[92:93]:
+
+        url = urllib.parse.urlsplit(url)
+        url = urllib.parse.urlunsplit(url._replace(path=urllib.parse.quote(url.path)))
+        print(url)
+        #create a request object
+        req = urllib.request.Request(url, headers=_HDR)
+
+        team_page = urllib.request.urlopen(req).read()
 
         for row in BeautifulSoup(team_page, 'html.parser').find_all('tr')[2:]:
             current_row = row.find_all('td')
